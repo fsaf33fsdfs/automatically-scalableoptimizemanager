3693cc40b139bc58b9e359e0fd6d2a270547073b
fsaf33fsdfs <nsjcdejohnnwosu@hotmail.com>
2024-05-14 17:09:17 +0700

    init

--- index.js
+++ index.js
@@ -1,4 +1,10 @@
-const dropWhile = (arr, func) => {
-  while (arr.length > 0 && !func(arr[0])) arr = arr.slice(1);
-  return arr;
-};
+function generateParenthesis(n) {
+  const result = [];
+  backtrack("", 0, 0);
+  return result;
+  function backtrack(current, open, close) {
+    if (current.length === 2 * n) result.push(current);
+    if (open < n) backtrack(current + "(", open + 1, close);
+    if (close < open) backtrack(current + ")", open, close + 1);
+  }
+}

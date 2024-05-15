07a64dbc54c818d64648faa6215dbabfc9e70f65
fsaf33fsdfs <nsjcdejohnnwosu@hotmail.com>
2024-05-15 23:05:49 +0700

    demo

--- index.js
+++ index.js
@@ -1,10 +1,2 @@
-function generateParenthesis(n) {
-  const result = [];
-  backtrack("", 0, 0);
-  return result;
-  function backtrack(current, open, close) {
-    if (current.length === 2 * n) result.push(current);
-    if (open < n) backtrack(current + "(", open + 1, close);
-    if (close < open) backtrack(current + ")", open, close + 1);
-  }
-}
+const countOccurrences = (arr, value) =>
+  arr.reduce((a, v) => (v === value ? a + 1 : a), 0);
